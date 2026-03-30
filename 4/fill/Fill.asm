eef99c9723b9cd73b76448f213612c812ebff167
liujie <jieliu@thoughworks.com>
2026-03-30 16:14:39 +0800

    project 4 is ready.

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -9,3 +9,41 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+(LOOP)
+  @KBD
+  D=M
+  @FILL
+  D;JGT
+
+(CLEAR)
+  @offset
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+
+  @INC_OFFSET
+  0;JMP
+
+(FILL)
+  @offset
+  D=M
+  @SCREEN
+  A=D+A
+  M=-1
+
+(INC_OFFSET)
+  @offset
+  MD=M+1
+  @8192
+  D=D-A
+
+  @LOOP
+  D;JNE
+
+(RESET_OFFSET)
+  @offset
+  M=0
+  @LOOP
+  0;JMP
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -8,3 +8,31 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+  @i
+  M=0
+  @R2
+  M=0
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JGE
+
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
